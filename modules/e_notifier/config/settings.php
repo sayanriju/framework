bc7d097a9ce392f2182d1720e212deced892f284
Ersin Guvenc <eguvenc@gmail.com>
2011-10-24 10:15:56 +0300

    removed e_notifier test smtp settings

--- modules/e_notifier/config/settings.php
+++ modules/e_notifier/config/settings.php
@@ -9,14 +9,14 @@
 $config['send_errors']    = TRUE;
 $config['write_log']      = FALSE;  // Keep all errors into log file.
 
-$config['recipients']     = array('eguvenc@gmail.com');
-$config['from_email']     = 'news@develturk.com';
+$config['recipients']     = array('');
+$config['from_email']     = 'me@example.com';
 $config['from_name']      = 'Obullo Application Errors';
 $config['subject']        = 'Application Errors';
 
 $config['protocol']       = 'smtp';
-$config['smtp_host']      = 'mail.develturk.com';   // pust ssl://  for ssl connection
-$config['smtp_user']      = 'news@develturk.com';
-$config['smtp_pass']      = '99219033';
+$config['smtp_host']      = 'mail.example.com';   // pust ssl://  for ssl connection
+$config['smtp_user']      = '';
+$config['smtp_pass']      = '';
 $config['smtp_port']      = '587'; // 465 for ssl
 $config['smtp_timeout']   = '21';
